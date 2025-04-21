96474c48eaae5f92fb0cccc35fc1434760c139e0
nyfeu <andre.maiolini@hotmail.com>
2025-04-21 12:44:43 -0300

    docs(tasks): adequar documentacao da classe TaskBattery

--- include/Tasks/TaskBattery.hpp
+++ include/Tasks/TaskBattery.hpp
@@ -6,15 +6,19 @@
 //--------------------------------------------------------------------------------
 //   TaskBattery Parameters
 
-#define BATTERY_PRIORITY 1          // Prioridade da task
-#define BATTERY_DELAY 5000          // vTaskDelay em ms
-#define BATTERY_STACK 4096          // Tamanho da stack (bytes)
+/// Prioridade da TaskBattery
+#define BATTERY_PRIORITY 1          
+
+/// Delay da TaskBattery em [ms]
+#define BATTERY_DELAY 5000          
+
+/// Tamanho da stack da TaskBattery em [bytes]
+#define BATTERY_STACK 4096          
 
 /**
  * @class TaskBattery
  * @brief Classe responsável pela verificação do nível de tensão da bateria.
- * 
- * Esta classe encapsula as funcionalidades da medoção do nível de tensão da bateria,
+ * @details Esta classe encapsula as funcionalidades da medoção do nível de tensão da bateria,
  * via ADC (Analog to Digital Converter).
  */
 
@@ -22,24 +26,24 @@ class TaskBattery {
 
     public:
 
-        /// Handle da task 
+        /// Handle da TaskBattery
         static TaskHandle_t taskHandle;
 
         /**
          * @brief Inicia a task de verificação do nível de bateria
-         * 
-         * Esta função cria a task responsável pela verificação da
+         * @details Esta função cria a task responsável pela verificação da
          * tensão da bateria, via ADC (Analog to Digital Converter).
          * Resultando no nível de bateria.
+         * @return void
          */
 
         static void start();
 
         /**
          * @brief Esta é a função executada pela task.
-         * 
          * @param pvParameters Parâmetros fornecidos pela função xTaskCreate,
          *        normalmente utilizados para passar dados para a task.
+         * @return void
          */
 
         static void taskFunction(void *pvParameters);
